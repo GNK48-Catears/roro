b79594c7568f3ef467ff50cfa4b2c5c3b2327a85
GNK48-Catears <877819165@qq.com>
2025-09-20 01:13:29 -0400

    Update FunctionEventBus.cs
    write documentation for two event

--- Assets/Scripts/Core/FunctionEventBus.cs
+++ Assets/Scripts/Core/FunctionEventBus.cs
@@ -3,6 +3,12 @@ using System;
 
 public class FunctionEventBus
 {
+    /// <summary>
+    /// float: current score
+    /// </summary>
     public static Action<float> UserScoreUpdated;
+    /// <summary>
+    /// float: the amount of score added in this single action
+    /// </summary>
     public static Action<float> UserScoreAdded;
 }
